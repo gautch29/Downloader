76619ec143b859e1198ea4ce09c6a315d384c361
Gauthier BARON <gauthier.baron@icloud.com>
2025-11-25 01:04:35 +0100

    build fix

--- types/zt-film-api.d 2.ts
+++
@@ -1,8 +0,0 @@
-// Type declarations for zt-film-api
-declare module 'zt-film-api' {
-    export default class ZTP {
-        static useBaseURL(url?: string): Promise<void>;
-        static search(category: string, query: string): Promise<any[]>;
-    }
-}
-
